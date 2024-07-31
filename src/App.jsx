dc1c129c575f9eabbbd036c109f08307a5bf510f
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-07-31 13:55:54 +0900

    App.jsx 파일 추가
    - main.jsx에서 App.jsx import 하기

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// React를 import 해주지 않으면, React is not defined 오류 발생
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React 웹 앱</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,15 @@
-console.log('ㅎㅇ');
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('react-app');
+
+if (container) {
+  createRoot(container).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.warn('문서에 "#react-app" 요소가 존재하지 않습니다.');
+}
