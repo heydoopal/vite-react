3cdb784d687c5af61573b3d8bc9b91264255c431
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-07-31 16:59:30 +0900

    eslint.config 파일 수정
    - eslint-plugin-react-hooks/refresh plugins 연결 및 rules 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
@@ -14,6 +16,8 @@ export default [
     },
     plugins: {
       react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      'react-refresh': pluginReactRefresh,
     },
   },
   {
@@ -33,6 +37,8 @@ export default [
   pluginReact.configs.flat.recommended,
   {
     rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'warn',
       'react/react-in-jsx-scope': 'off',
     },
   },
